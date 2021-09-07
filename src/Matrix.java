c815500730506de73f9e927d2070ce43188dffcb
fatrisiamarpaung <fatrisiamarpaung08@gmail.com>
2021-09-07 22:09:37 +0700

    LFixing Logic

--- src/Matrix.java
+++ src/Matrix.java
@@ -1,30 +1,65 @@
+import java.util.Scanner;
+
 public class Matrix {
 
     public static void main(String[] args) {
-        Sum();
-    }
 
-    public static int[][] Sum() {
-        int i, j = 0;
-        int[][] firstMatrix = {{2,3,4}, {5,2,3}};
-        int[][] secondMatrix = {{-4,5,3},{5,6,3}};
-        int[][] sumMatrix = new int[2][3];
+        int row, col, i, j;
+        Scanner scanner = new Scanner(System.in);
 
-        for (i=0; i<2; i++){
-            for (j=0; j<3; j++){
-                sumMatrix[i][j] =firstMatrix[i][j] + secondMatrix[i][j];
+        System.out.println("Input Row Number:");
+        row = scanner.nextInt();
+
+        System.out.println("Input Column Number: ");
+        col = scanner.nextInt();
+
+        int[][] firstMatrix = new int[row][col];
+        int[][] secondMatrix = new int[row][col];
+
+        System.out.println("Input Matrix 1:");
+        for (i=0; i<row; i++){
+            for (j=0; j<col; j++){
+                firstMatrix[i][j] = scanner.nextInt();
             }
         }
 
+        System.out.println("Input Matrix 2:");
+        for (i=0; i<row; i++){
+            for (j=0; j<col; j++){
+                secondMatrix[i][j] = scanner.nextInt();
+            }
+        }
+
+        int resultMatrix[][];
+        resultMatrix = sumMatrix(row, col, firstMatrix, secondMatrix);
+        printMatrix(resultMatrix, row, col);
+    }
+
+    static int[][] sumMatrix(int row, int col, int[][] firstMatrix, int[][] secondMatrix) {
+
+        int i, j = 0;
+
+        int[][] newMatrix = new int[row][col];
+
+        for (i=0; i<row; i++){
+            for (j=0; j<col; j++){
+                newMatrix[i][j] = firstMatrix[i][j] + secondMatrix[i][j];
+            }
+        }
+
+        return newMatrix;
+    }
+
+    static void printMatrix(int[][] matrix, int row, int col){
+        int i, j;
         System.out.println("Sum of two matrices is: ");
 
-        for (i=0; i<2; i++){
-            for (j=0; j<3; j++){
-                System.out.print(+(sumMatrix[i][j])+" ");
+        for (i=0; i<row; i++){
+            for (j=0; j<col; j++){
+                System.out.print(+(matrix[i][j])+" ");
             }
             System.out.println();
         }
-
-        return sumMatrix;
     }
 }
+
